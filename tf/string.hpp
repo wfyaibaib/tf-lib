d02de1e39979156043a4612e5aa0442cf2a4efc4
wfyaibaib <wfyaibaib@gmail.com>
2013-08-04 22:34:07 +0800

    add some functions about string

--- tf/string.hpp
+++ tf/string.hpp
@@ -2,6 +2,7 @@
 #define STRING_HPP
 #include <string>
 #include <sstream>
+#include <cstdlib>
 namespace tf
 {
 template <class T>
@@ -10,6 +11,33 @@ std::string to_string(const T& t)
     std::stringstream stream;
     stream << t;
     return stream.str();
+}
+template <class Re>
+Re stringTo(string str)
+{
+
+    std::istringstream in(str);
+    Re re;
+    in >> re;
+    return re;
+}
+inline string upperCase(const string& s)
+{
+
+    std::string upper(s);
+    for (size_t i = 0; i < s.length(); i++)
+        upper[i] = toupper(upper[i]);
+    return upper;
+}
+
+inline string lowerCase(const string& s)
+{
+
+    std::string lower(s);
+    for (size_t i = 0; i < s.length(); i++)
+        lower[i] = tolower(lower[i]);
+
+    return lower;
 }
 
 }
